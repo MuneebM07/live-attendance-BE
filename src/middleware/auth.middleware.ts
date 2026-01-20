3a8287371a57165e307bfa03a8bf618a74a0fea1
MuneebM07 <muneeb.dev01@gmail.com>
2026-01-21 01:31:25 +0530

    middleware

---
+++ src/middleware/auth.middleware.ts
@@ -0,0 +1,41 @@
+import type { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+
+
+const JWT_PASSWORD = process.env.JWT_PASSWORD as string;
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const header = req.headers["authorization"];
+
+    if (!header) {
+
+        return res.status(401).json({
+            success: false,
+            error: "Unauthorized, token missing or invalid"
+        });
+
+    }
+    try {
+        const decoded = jwt.verify(header, JWT_PASSWORD);
+        if (typeof decoded === "string") {
+            return res.status(401).json({
+                success: false,
+                error: "Unauthorized, token missing or invalid"
+
+            })
+        }
+        (req as any).user = decoded
+
+        next()
+
+    }
+    catch (error) {
+        return res.status(401).json({
+            success: false,
+            error: "Unauthorized, token missing or invalid"
+        })
+
+    }
+
+
+}
